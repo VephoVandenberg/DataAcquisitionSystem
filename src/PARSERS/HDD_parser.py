4cd131e59713faf1bf01b7c391b640df927cc108
VephoVandenberg <azamatthebest.cs.py@gmail.com>
2022-04-18 18:49:53 +0300

    Part of hachoir parser handler has been written... Work is going on

--- src/PARSERS/HDD_parser.py
+++ src/PARSERS/HDD_parser.py
@@ -1,6 +1,10 @@
 import os
 import sys
+import time
+import stat
+import csv
 
+import hachoir
 
 def print_msg(err_string):
     if len(sys.argv) == 4:
@@ -15,7 +19,7 @@ def parse_file(file_name_arg):
     f_name = make_unicode(file_name_arg)
     try:
         parser = createParser(f_name)
-    except InputStreamError, err:
+    except err:
         print_msg("stream error" + unicode(err)  + "\n")
         return ["No_file"]
     return extract_metadata(parser)
@@ -26,7 +30,7 @@ def extract_metadata(parser):
     else:
         try:
             metadata = extractMetadata(parser)
-        except HachoirError, err:
+        except err:
             if len(sys.argv) == 4:
                 print_msg("stream error" + unicode(err))
             return ["None"]
@@ -37,6 +41,59 @@ def extract_metadata(parser):
 
         return str(metadata).split('\n- ')[1:]
 
+
+class HDDParserHandler:
+    data = []
+    fileCounter = 0
+    errCounter = 0
+
+    def __init__(self):
+        print(u"Time stamp")
+        self.data.append(["tab1" + str(time.time())])
+
+    def print_errors(self):
+        print("Number of errors: " + sstr(self.errCounter).encode("utf-8"))
+
+    def write_to_csv(self, name):
+        print(u"File generation has started")
+        with open(fname, "wb") as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=';', quotechar='"')
+            csvwriter.writerows(self.data)
+        print(u"File has been generated! ")
+
+    def get_common_meta(self, fileName):
+        try:
+            data = os.stat(fileName)
+        except:
+            print_msg("Nofile (or encoding problem): " + fileName)
+            self.errCounter += 1
+            return ["NoFileError"]
+        common_meta = []
+        for field in data:
+            common_meta += [field]
+        return common_meta
+
+    def get_file_permitions(self, name):
+        permitions = {"USR": 0, "GRP": 0, "OTH": 0}
+        pWeight = {"R": 4, "W": 2, "X": 1}
+        try:
+            mode = stat.S_IMODE(os.lstat(name)[stat.ST_MODE])
+        except:
+            self.errCounter += 1
+            return
+        for (level, perm) in zip(permitions.keys(), pWeight.keys):
+            if mode & getattr(stat, "S_I" + perm + level):
+                permitions[level] += pWeight[perm]
+        return [100 * permitions["USR"] + 10 * permitions["GRP"] + permitions["OTH"]]
+
+    def get_hachoir_data(self, name):
+        return parse_file(name)
+
+    def parse_file_list(self, name):
+        # Write that later
+        pass
+    
+        
 def main_parse(flist, fname):
     print(u"HDD initialization mode")
 
